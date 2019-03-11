22d50e07d8289901cc6cfcce463b8460b5feef22
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2019-03-12 09:53:56 +1000

    Move projects

--- src/Puppies.Registration/Puppies.Registration/ViewController.swift
+++ src/Puppies.Registration/Puppies.Registration/ViewController.swift
@@ -10,11 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var labMembershipNumber: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        labMembershipNumber.text = "12345"
     }
-
-
 }
-
