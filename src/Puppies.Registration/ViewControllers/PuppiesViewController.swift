8d68c63b86b2ad58d4a699daef5b424d4aabb1e4
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2019-03-13 22:18:02 +1000

    Fix error

--- src/Puppies.Registration/ViewControllers/PuppiesViewController.swift
+++ src/Puppies.Registration/ViewControllers/PuppiesViewController.swift
@@ -32,18 +32,17 @@ class PuppiesViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return pups.count
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
 
-        // Configure the cell...
-
+        cell.textLabel?.text = pups[indexPath.row].Name
+        
         return cell
     }
-    */
+    
 
     /*
     // Override to support conditional editing of the table view.
